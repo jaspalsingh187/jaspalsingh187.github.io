545e825109164c18faa8c7886c0711f8a3c0e214
jaspalsingh187 <jaspalsinghjs0313@gmail.com>
2025-08-03 21:02:06 -0400

    Lab4 part 4
    Made some changes

--- lab4/part4/main-finished.js
+++ lab4/part4/main-finished.js
@@ -1,4 +1,5 @@
-// setup canvas
+// set up canvas
+
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
@@ -6,16 +7,17 @@ const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
 // function to generate random number
+
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// function to generate random color
+// function to generate random RGB color value
+
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
-// Define Ball class
 class Ball {
   constructor(x, y, velX, velY, color, size) {
     this.x = x;
@@ -34,12 +36,20 @@ class Ball {
   }
 
   update() {
-    if (this.x + this.size >= width || this.x - this.size <= 0) {
-      this.velX = -this.velX;
+    if (this.x + this.size >= width) {
+      this.velX = -Math.abs(this.velX);
     }
 
-    if (this.y + this.size >= height || this.y - this.size <= 0) {
-      this.velY = -this.velY;
+    if (this.x - this.size <= 0) {
+      this.velX = Math.abs(this.velX);
+    }
+
+    if (this.y + this.size >= height) {
+      this.velY = -Math.abs(this.velY);
+    }
+
+    if (this.y - this.size <= 0) {
+      this.velY = Math.abs(this.velY);
     }
 
     this.x += this.velX;
@@ -48,7 +58,7 @@ class Ball {
 
   collisionDetect() {
     for (const ball of balls) {
-      if (this !== ball) {
+      if (!(this === ball)) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
@@ -61,14 +71,15 @@ class Ball {
   }
 }
 
-// Create balls
 const balls = [];
 
 while (balls.length < 25) {
   const size = random(10, 20);
   const ball = new Ball(
-    random(size, width - size),
-    random(size, height - size),
+    // ball position always drawn at least one ball width
+    // away from the edge of the canvas, to avoid drawing errors
+    random(0 + size, width - size),
+    random(0 + size, height - size),
     random(-7, 7),
     random(-7, 7),
     randomRGB(),
@@ -78,7 +89,6 @@ while (balls.length < 25) {
   balls.push(ball);
 }
 
-// Animation loop
 function loop() {
   ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
   ctx.fillRect(0, 0, width, height);
