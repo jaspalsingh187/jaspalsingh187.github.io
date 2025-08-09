202e2a1900b1106534bd95fabc336b766ff4bddd
jaspalsingh187 <jaspalsinghjs0313@gmail.com>
2025-08-08 23:35:44 -0400

    lab5
    extracted files

--- lab4/part4/main-finished.js
+++ lab4/part4/main-finished.js
@@ -1,5 +1,3 @@
-
-
 const para = document.querySelector('p');
 let count = 0;
 
@@ -9,33 +7,26 @@ const ctx = canvas.getContext('2d');
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 
-function random(min,max) {
-  const num = Math.floor(Math.random()*(max-min)) + min;
-  return num;
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
 };
-
-// function to CREATE random RGB color value
 
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
 class Shape {
-
   constructor(x, y, velX, velY) {
     this.x = x;
     this.y = y;
     this.velX = velX;
     this.velY = velY;
   }
-
 }
 
 class Ball extends Shape {
-
   constructor(x, y, velX, velY, color, size) {
     super(x, y, velX, velY);
-
     this.color = color;
     this.size = size;
     this.exists = true;
@@ -49,19 +40,11 @@ class Ball extends Shape {
   }
 
   update() {
-    if ((this.x + this.size) >= width) {
+    if ((this.x + this.size) >= width || (this.x - this.size) <= 0) {
       this.velX = -(this.velX);
     }
 
-    if ((this.x - this.size) <= 0) {
-      this.velX = -(this.velX);
-    }
-
-    if ((this.y + this.size) >= height) {
-      this.velY = -(this.velY);
-    }
-
-    if ((this.y - this.size) <= 0) {
+    if ((this.y + this.size) >= height || (this.y - this.size) <= 0) {
       this.velY = -(this.velY);
     }
 
@@ -69,45 +52,33 @@ class Ball extends Shape {
     this.y += this.velY;
   }
 
-
   collisionDetect() {
-     for (const ball of balls) {
-        if (!(this === ball) && ball.exists) {
-           const dx = this.x - ball.x;
-           const dy = this.y - ball.y;
-           const distance = Math.sqrt(dx * dx + dy * dy);
+    for (const ball of balls) {
+      if (!(this === ball) && ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
 
-           if (distance < this.size + ball.size) {
-             ball.color = this.color = randomRGB();
-           }
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
         }
-     }
+      }
+    }
   }
-
 }
 
 class EvilCircle extends Shape {
-
   constructor(x, y) {
     super(x, y, 20, 20);
-
     this.color = "white";
     this.size = 10;
 
     window.addEventListener('keydown', (e) => {
-      switch(e.key) {
-        case 'a':
-          this.x -= this.velX;
-          break;
-        case 'd':
-          this.x += this.velX;
-          break;
-        case 'w':
-          this.y -= this.velY;
-          break;
-        case 's':
-          this.y += this.velY;
-          break;
+      switch (e.key) {
+        case 'a': this.x -= this.velX; break;
+        case 'd': this.x += this.velX; break;
+        case 'w': this.y -= this.velY; break;
+        case 's': this.y += this.velY; break;
       }
     });
   }
@@ -121,21 +92,10 @@ class EvilCircle extends Shape {
   }
 
   checkBounds() {
-    if ((this.x + this.size) >= width) {
-      this.x -= this.size;
-    }
-
-    if ((this.x - this.size) <= 0) {
-      this.x += this.size;
-    }
-
-    if ((this.y + this.size) >= height) {
-      this.y -= this.size;
-    }
-
-    if ((this.y - this.size) <= 0) {
-      this.y += this.size;
-    }
+    if ((this.x + this.size) >= width) this.x -= this.size;
+    if ((this.x - this.size) <= 0) this.x += this.size;
+    if ((this.y + this.size) >= height) this.y -= this.size;
+    if ((this.y - this.size) <= 0) this.y += this.size;
   }
 
   collisionDetect() {
@@ -153,18 +113,13 @@ class EvilCircle extends Shape {
       }
     }
   }
-
 }
-
-// define array to store balls and populate it
 
 const balls = [];
 
 while (balls.length < 25) {
   const size = random(10, 20);
   const ball = new Ball(
-    // ball position always drawn at least one ball width
-    // away from the edge of the canvas, to avoid drawing errors
     random(0 + size, width - size),
     random(0 + size, height - size),
     random(-7, 7),
